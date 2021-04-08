949dd59757cc2a734f7af0b4118986abd72c7233
ckheav@terpmail.umd.edu <52798523+ckheav@users.noreply.github.com>
2021-04-07 23:19:02 -0400

    Weather database added

--- server.js
+++ server.js
@@ -14,26 +14,6 @@ app.use(express.json());
 
 app.use('/api', apiRoutes);
 app.use(express.static(staticFolder));
-import {
-  Sequelize
-} from "sequelize";
-const sequelize = new Sequelize({
-  username: 'student',
-  password: 'INST377@UMD',
-  host:'3.236.243.212',
-  database:'weather_db',
-  dialect: 'mysql',
-  ssl: 'Amazon RDS',
-  pool: {maxConnections: 5, maxIdleTime: 30},
-  language: 'en'
-  
-});
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
 
 async function bootServer() {
   try {
