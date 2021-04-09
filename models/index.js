44024c5fd6b3c0d5ad07848d85c6ca6f0e442c69
Martin Noren <55893214+M-Noren@users.noreply.github.com>
2021-04-08 22:15:05 -0400

    Finished JS and CSS changes

--- models/index.js
+++ models/index.js
@@ -2,12 +2,12 @@ import DietaryRestrictions from './DietaryRestrictions.js';
 import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
-import Weather from './Weather'; 
+import Weather from './Weather.js'; 
 
 export default {
   DietaryRestrictions,
   DiningHall,
   Macros,
   Meals,
-  Weather
+  Weather,
 };
