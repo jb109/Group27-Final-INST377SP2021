b10dc02219db37f969abb06a22b76e60fdf973d6
ckheav@terpmail.umd.edu <52798523+ckheav@users.noreply.github.com>
2021-04-21 09:17:08 -0400

    Removing all Dining hall models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,5 @@
-import DietaryRestrictions from './DietaryRestrictions.js';
-import DiningHall from './DiningHall.js';
-import Macros from './Macros.js';
-import Meals from './Meals.js';
 import WeatherPrimary from './Weather_primary'; 
 
 export default {
-  DietaryRestrictions,
-  DiningHall,
-  Macros,
-  Meals,
-  WeatherPrimary,
+  WeatherPrimary
 };
