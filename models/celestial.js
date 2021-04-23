792f49a9af3775dc086bc2108af2b076989bb3c8
Humdan1 <36046285+Humdan1@users.noreply.github.com>
2021-04-23 11:52:18 -0400

    Create celestial.js

---
+++ models/celestial.js
@@ -0,0 +1,33 @@
+export default (sequelize, DataTypes) => {
+  const Phases = sequelize.define(
+    'Celestial Phases',
+    {
+      celestial_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+        primaryKey: true
+      },
+      moon_type: {
+        type: DataTypes.VARCHAR45
+      },
+      moon_rise_time: {
+        type: DataTypes.TIME
+      },
+      moon_fall_time: {
+        type: DataTypes.TIME
+      },
+      sun_rise_time: {
+        type: DataTypes.TIME
+      },
+      sun_fall_time: {
+        type: DataTypes.TIME
+      },
+      sea_info_id: {
+        type: DataTypes.INTEGER
+      },
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return Meals;
+};
